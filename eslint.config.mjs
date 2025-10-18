3c6ab829b9c348a62bd0cfd3bb0ebd9e32010b41
Jai <jaipkapoor99@gmail.com>
2025-10-18 20:57:35 +0530

    feat: enhance CI workflow with caching for Next.js build artifacts; streamline environment variable setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,15 +8,10 @@ export default defineConfig([
   {
     ignores: ["build/**", ".next/**", "next-env.d.ts"]
   },
+  nextPlugin.flatConfig.coreWebVitals,
   ...typescriptConfigs,
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    plugins: {
-      "@next/next": nextPlugin
-    },
     rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
       "react/jsx-key": "off"
     }
   }
